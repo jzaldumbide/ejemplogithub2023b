a96839976a851cbed715de8683750963f821bed3
Juan Zaldumbide <jp@Juans-MacBook-Pro.local>
2023-11-14 09:51:35 -0500

    Initial commit

---
+++ src/ejemploClases.java
@@ -0,0 +1,12 @@
+public class ejemploClases {
+
+    int ojos;
+    String nombre;
+
+    public ejemploClases(int ojos, String nombre) {
+        this.ojos = ojos;
+        this.nombre = nombre;
+    }
+
+
+}
